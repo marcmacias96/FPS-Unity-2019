4cbb5641b50d6047650298094b3e3a56aa4db302
marcmacias96 <mamacias3@espe.edu.ec>
2019-12-03 10:45:27 -0500

    Primer

---
+++ Assets/Scripts/destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Debug.Log("Hola");
+    }
+}
